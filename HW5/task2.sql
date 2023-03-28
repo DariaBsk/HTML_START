eade61ba015d5f7ab3961b52ea92b2d40e68978a
DariaBsk <Dariabsk602@gmail.com>
2023-03-28 21:20:36 +0300

    Добавление домашней работы 5

---
+++ HW5/task2.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE EMPLOYEE (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+age TEXT NOT NULL,
+address TEXT NOT NULL
+);
+
+INSERT INTO EMPLOYEE VALUES (001, 'Иван', '35', 'Москва');
+INSERT INTO EMPLOYEE VALUES (002, 'Александр', '41', 'Молодечно');
+INSERT INTO EMPLOYEE VALUES (003, 'Михаил', '43', 'Москва');
+INSERT INTO EMPLOYEE VALUES (004, 'Даниил', '25', 'Пермь');
+INSERT INTO EMPLOYEE VALUES (005, 'Сергей', '36', 'Минск');
+INSERT INTO EMPLOYEE VALUES (006, 'Анна', '31', 'Москва');
+INSERT INTO EMPLOYEE VALUES (007, 'Дмитрий', '34', 'Санкт-Петербург');
+INSERT INTO EMPLOYEE VALUES (008, 'Андрей', '28', 'Тула');
+INSERT INTO EMPLOYEE VALUES (009, 'Юрий', '24', 'Тверь');
+INSERT INTO EMPLOYEE VALUES (010, 'Илья', '30', 'Орел');
